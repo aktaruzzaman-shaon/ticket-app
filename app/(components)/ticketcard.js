f025204f83a66cc9b73a480769619a722c8b97a6
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2024-02-21 14:09:37 +0600

    initial design of ticket card

--- app/(components)/ticketcard.js
+++ app/(components)/ticketcard.js
@@ -1,7 +1,35 @@
+import Deleteblock from "./deleteblock"
+import Prioritydisplay from "./prioritydisplay"
+import Progreesdisplay from "./progreesdisplay"
+import Statusdisplay from "./statusdisplay"
 
 const Ticketcard = () => {
   return (
-    <div>ticketcard</div>
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+
+      <div className="flex mb-3">
+        <Prioritydisplay></Prioritydisplay>
+        <div className="ml-auto">
+          <Deleteblock></Deleteblock>
+        </div>
+
+      </div>
+
+      <h4 >Tciket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">THis is the ticket description</p>
+      <div className="flex-grow"></div>
+
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1 ">08/31 10.43 PM</p>
+          <Progreesdisplay></Progreesdisplay>
+        </div>
+        <div className="ml-auto flex items-end">
+          <Statusdisplay></Statusdisplay>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
+import Ticketcard from "./(components)/ticketcard";
 
 export default function DashBoard() {
   return (
-    <div>Dashboard</div>
+    <div className="p-5 ">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <Ticketcard></Ticketcard>
+        <Ticketcard></Ticketcard>
+        <Ticketcard></Ticketcard>
+      </div>
+    </div>
   );
 }
