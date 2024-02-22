12fa5fabfd7fb1b2b4c088a514c03db20f474367
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2024-02-22 21:51:35 +0600

    dynamic ticket card

--- app/(components)/progreesdisplay.js
+++ app/(components)/progreesdisplay.js
@@ -1,8 +1,8 @@
 
-const Progreesdisplay = () => {
+const Progreesdisplay = ({progress}) => {
     return (
         <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "75%" }}>
+            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${progress}` }}>
                 
             </div>
         </div>
--- app/(components)/ticketcard.js
+++ app/(components)/ticketcard.js
@@ -3,30 +3,30 @@ import Prioritydisplay from "./prioritydisplay"
 import Progreesdisplay from "./progreesdisplay"
 import Statusdisplay from "./statusdisplay"
 
-const Ticketcard = () => {
+const Ticketcard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
 
       <div className="flex mb-3">
-        <Prioritydisplay></Prioritydisplay>
+        <Prioritydisplay priority={ticket.priority}></Prioritydisplay>
         <div className="ml-auto">
           <Deleteblock></Deleteblock>
         </div>
 
       </div>
 
-      <h4 >Tciket Title</h4>
+      <h4 >{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">THis is the ticket description</p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
 
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1 ">08/31 10.43 PM</p>
-          <Progreesdisplay></Progreesdisplay>
+          <p className="text-xs my-1 ">{ticket.createdAt}</p>
+          <Progreesdisplay progress={ticket.progress}></Progreesdisplay>
         </div>
         <div className="ml-auto flex items-end">
-          <Statusdisplay></Statusdisplay>
+          <Statusdisplay status={ticket.status}></Statusdisplay>
         </div>
       </div>
     </div>
